784e59e619e87067f1cd983bba23e13a3acb50bc
n3rd <godwinjdayo@gmail.com>
2023-10-15 21:42:49 +0100

    👌 IMPROVE: improved modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,18 +1,68 @@
 <script>
 	import { modalOpen } from './stores';
+	import clipboard from 'clipboardy';
+	import UrlInfo from './sub-components/UrlInfo.svelte';
+
+	let link = '';
+
+	const paste = async () => {
+		let text = await clipboard.read();
+		link = text;
+	};
+
+	const isValidUrl = (/** @type {string} */ urlString) => {
+		var urlPattern = new RegExp(
+			'^(https?:\\/\\/)?' + // validate protocol
+				'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+				'((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+				'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+				'(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+				'(\\#[-a-z\\d_]*)?$',
+			'i'
+		); // validate fragment locator
+		return !!urlPattern.test(urlString);
+	};
 </script>
 
 {#if $modalOpen}
 	<dialog id="my_modal_5" class="modal modal-open modal-bottom sm:modal-middle">
 		<div class="modal-box">
-			<h3 class="font-bold text-lg">Hello!</h3>
+			<h3 class="font-bold text-lg">Paste a link</h3>
 			<p class="py-4">Press ESC key or click the button below to close</p>
-			<div class="modal-action">
-				<form method="dialog">
+			<input
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered w-full"
+				bind:value={link}
+			/>
+			<div class="modal-action flex justify-between w-full">
+				<div>
+					<button class="btn btn-primary" on:click={() => paste()}
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="#ffffff"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							><path
+								d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
+							/><rect x="8" y="2" width="8" height="4" rx="1" ry="1" /></svg
+						></button
+					>
+				</div>
+				<form method="dialog" class="flex gap-3">
 					<!-- if there is a button in form, it will close the modal -->
 					<button class="btn" on:click={() => modalOpen.set(false)}>Close</button>
+					<button class="btn btn-primary">Get QRcode</button>
 				</form>
 			</div>
+			{#if !isValidUrl(link)}
+				<UrlInfo />
+			{/if}
 		</div>
 	</dialog>
 {/if}
---
+++ src/lib/sub-components/UrlInfo.svelte
@@ -0,0 +1,25 @@
+<div
+	class="link-info py-3 flex gap-2 items-center text-current tooltip tooltip-right w-fit"
+	data-tip="(optional) current input value is not a url"
+>
+	<div>Not a valid url</div>
+	<div>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="16"
+			height="16"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="#ffffff"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			><circle cx="12" cy="12" r="10" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><line
+				x1="12"
+				y1="17"
+				x2="12.01"
+				y2="17"
+			/></svg
+		>
+	</div>
+</div>
