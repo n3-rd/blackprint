b041567cabb2c93c2f6f369313af8c49797e31f5
n3rd <godwinjdayo@gmail.com>
2023-10-20 00:28:49 +0100

    👌 IMPROVE: Removed console logs

--- src/lib/ColorModal.svelte
+++ src/lib/ColorModal.svelte
@@ -1,12 +1,6 @@
 <script>
-	import { backgroundColor, foregroundColor } from './stores';
 	import BackgroundPicker from './sub-components/BackgroundPicker.svelte';
 	import ForegroundPicker from './sub-components/ForegroundPicker.svelte';
-
-	setInterval(() => {
-		console.log('back', $backgroundColor);
-		console.log('fore', $foregroundColor);
-	}, 1000);
 </script>
 
 <h3 class="font-bold text-lg">Colors</h3>
